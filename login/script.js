4b4d845d90c44490b59fb437e72ab68b72c5ecbf
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2021-12-28 15:11:41 +0100

    Update script.js

--- login/script.js
+++ login/script.js
@@ -5,37 +5,38 @@ localStorage.getItem("registered") && localStorage.removeItem("registered");
 const form2 = document.getElementById("login");
 form2.addEventListener("submit", login);
 if (localStorage.getItem("token")) {
-    window.location.href = window.location.href + "/"
-};
+  window.location.href = window.location.href + "/";
+}
 
 async function login(event) {
-    event.preventDefault();
-    const username = event.target.children[0].value;
-    const password = event.target.children[1].value;
+  event.preventDefault();
+  const username = event.target.children[0].value;
+  const password = event.target.children[1].value;
 
-    const result = await fetch("https://rest-api-thsx.herokuapp.com/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username,
-            password,
-        }),
-    }).then((res) => res.json());
+  const result = await fetch("https://rest-api-thsx.herokuapp.com/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  }).then((res) => res.json());
 
-    if (result.status === "ok") {
-        // everythign went fine
-        // console.log("Got the token: ", result.data);
-        localStorage.setItem("token", result.data);
-        localStorage.setItem(
-            "login",
-            document.querySelector("#login > input:nth-child(1)").value
-        );
-        // console.log(result);
-        new Popup("success", "Logged in successfully");
-        window.location.href = window.location.href + "/"
-    } else {
-        new Popup("error", result.error);
-    }
+  if (result.status === "ok") {
+    // everythign went fine
+    // console.log("Got the token: ", result.data);
+    localStorage.setItem("token", result.data);
+    localStorage.setItem(
+      "login",
+      document.querySelector("#login > input:nth-child(1)").value
+    );
+    // console.log(result);
+    new Popup("success", "Logged in successfully");
+    alert(window.location.href + "/");
+    window.location.href = window.location.href + "/";
+  } else {
+    new Popup("error", result.error);
+  }
 }
