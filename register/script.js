8cd671108cd62a3eb3e64a62579ffc1970617b56
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2021-12-28 14:26:09 +0100

    fix routes

--- register/script.js
+++ register/script.js
@@ -1,32 +1,34 @@
 import Popup from "../scripts/popUp.js";
 const form1 = document.getElementById("reg-form");
 form1.addEventListener("submit", registerUser);
+if (localStorage.getItem("token")) {
+    window.location.href = window.location.href + "/"
+};
 
 async function registerUser(event) {
-  event.preventDefault();
-  const username = event.target.children[0].value;
-  const password = event.target.children[1].value;
+    event.preventDefault();
+    const username = event.target.children[0].value;
+    const password = event.target.children[1].value;
 
-  const result = await fetch(
-    "https://rest-api-thsx.herokuapp.com/api/register",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
+    const result = await fetch(
+        "https://rest-api-thsx.herokuapp.com/api/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        }
+    ).then((res) => res.json());
+
+    if (result.status === "ok") {
+        // everythign went fine
+        new Popup("success", "successfully registered");
+        localStorage.setItem("registered", true);
+        window.location.href = window.location.href + "login/"
+    } else {
+        new Popup("error", result.error);
     }
-  ).then((res) => res.json());
-
-  if (result.status === "ok") {
-    // everythign went fine
-    new Popup("success", "successfully registered");
-    localStorage.setItem("registered", true);
-    window.location.replace("/login");
-  } else {
-    new Popup("error", result.error);
-  }
 }
